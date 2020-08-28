4306672cd86f386c1581436889b3a16dd9f0918d
Carmen Salas <carmensalas0114@gmail.com>
2020-08-27 23:57:25 -0400

    created an array of dat to simulate an api response

---
+++ customer-support-page/src/data/data.js
@@ -0,0 +1,47 @@
+//  Data for the icon tiles in the main section of the customer service page
+
+const iconTileData = [
+  {
+    icon_img: 'https://via.placeholder.com/70',
+    icon_img_alt: 'computer icon',
+    title: 'Your Orders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon_img: 'https://via.placeholder.com/70',
+    icon_img_alt: 'person icon',
+    title: 'Your Orders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon_img: 'https://via.placeholder.com/70',
+    icon_img_alt: 'Q&A icon',
+    title: 'Your Orders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon_img: 'https://via.placeholder.com/70',
+    icon_img_alt: 'truck icon',
+    title: 'Your Orders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon_img: 'https://via.placeholder.com/70',
+    icon_img_alt: 'box icon',
+    title: 'Your Orders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon_img: 'https://via.placeholder.com/70',
+    icon_img_alt: 'paper icon',
+    title: 'Your Orders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon_img: 'https://via.placeholder.com/70',
+    icon_img_alt: 'checkmark icon',
+    title: 'Your Orders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+];
+export default iconTileData;
