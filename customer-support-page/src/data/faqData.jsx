e67f53429a868d89df0b1ab39556f218f24cca80
Carmen Salas <carmensalas0114@gmail.com>
2020-08-28 02:07:27 -0400

    created array of objs to simulate api response data

---
+++ customer-support-page/src/data/faqData.jsx
@@ -0,0 +1,30 @@
+// Data for FAQ collapsable panels
+export const faqData = [
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nam rutrum quam quis justo sodales, at aliquet dui maximus.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nam rutrum quam quis justo sodales, at aliquet dui maximus.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nam rutrum quam quis justo sodales, at aliquet dui maximus.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nam rutrum quam quis justo sodales, at aliquet dui maximus.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nam rutrum quam quis justo sodales, at aliquet dui maximus.',
+  },
+];
+
+export default faqData;
